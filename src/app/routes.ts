bc13879396c7d0901596451799f412052080d01a
Filip Ślazyk <filip.slazyk@gmail.com>
2022-04-02 20:26:31 +0200

    Improve new request form

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,7 +1,9 @@
 import {Routes} from "@angular/router";
 import {MainComponent} from "./main/main.component";
+import {RequestFormComponent} from "./request-form/request-form.component";
 
 export const routes: Routes = [
   {path: 'main', component: MainComponent},
+  {path: 'new-request', component: RequestFormComponent},
   {path: '', redirectTo: '/main', pathMatch: 'full'},
 ];
