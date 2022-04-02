f4b9c5ba54be2d6799ac03ab7625614c73e5513c
Filip Ślazyk <filip.slazyk@gmail.com>
2022-04-02 17:03:05 +0200

    Add logging functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { MsalBroadcastService, MsalGuardConfiguration, MsalService, MSAL_GUARD_CONFIG } from '@azure/msal-angular';
+import { InteractionType, PopupRequest, AuthenticationResult, RedirectRequest, InteractionStatus } from '@azure/msal-browser';
+import { filter, takeUntil } from 'rxjs/operators';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +11,54 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'frontend';
+  loginDisplay = false;
+  private readonly _destroying$ = new Subject<void>();
+
+  constructor(
+    @Inject(MSAL_GUARD_CONFIG) private msalGuardConfig: MsalGuardConfiguration,
+    private authService: MsalService,
+    private msalBroadcastService: MsalBroadcastService
+  ) { }
+
+  ngOnInit(): void {
+
+    /**
+     * You can subscribe to MSAL events as shown below. For more info,
+     * visit: https://github.com/AzureAD/microsoft-authentication-library-for-js/blob/dev/lib/msal-angular/docs/v2-docs/events.md
+     */
+    this.msalBroadcastService.inProgress$
+      .pipe(
+        filter((status: InteractionStatus) => status === InteractionStatus.None),
+        takeUntil(this._destroying$)
+      )
+      .subscribe(() => {
+        this.setLoginDisplay();
+      });
+  }
+
+  setLoginDisplay() {
+    this.loginDisplay = this.authService.instance.getAllAccounts().length > 0;
+  }
+
+
+  login() {
+
+    if (this.msalGuardConfig.authRequest) {
+      this.authService.loginRedirect({ ...this.msalGuardConfig.authRequest } as RedirectRequest);
+    } else {
+      this.authService.loginRedirect();
+    }
+  }
+
+  logout() {
+    this.authService.logout();
+  }
+
+    // unsubscribe to events when component is destroyed
+    ngOnDestroy(): void {
+      this._destroying$.next(undefined);
+      this._destroying$.complete();
+    }
+
 }
+
